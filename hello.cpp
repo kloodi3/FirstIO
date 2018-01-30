d30c7170b8d391b8b03ed19897429d94f3f0e20f
kloodi3 <kmalrashdi@mail.csuchico.edu>
2018-01-29 17:20:14 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,4 @@
+// Author: Khaled Alrashdi
 #include <iostream>
 #include <string>
 
@@ -5,6 +6,6 @@ int main() {
     std::string input;
     std::cout << "What's your first name?" << std::endl;
     std::cin >> input;
-    std::cout << "Hello, " << input << std::endl;
+    std::cout << "Hello, my name is " << input << std::endl;
     return 0;
 }
